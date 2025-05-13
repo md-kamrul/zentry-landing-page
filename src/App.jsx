f35040e01e061bc55f0f0bad6b2486c4290d8e86
Md. Kamrul Islam <join.kamrul@gmail.com>
2025-05-13 14:48:20 +0600

    add custom fonts and color in indesx.css file and sucessfully tested customization in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold'>Welcome to page</h1>
+      <h1 className='text-5xl text-orange-500 font-bold bg-blue-300'>Welcome to page</h1>
     </main>
   );
 };
